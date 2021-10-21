62f7079e0239b263d71a1a0a6ba3edbc858f8d22
Vit Kabele <vit@kabele.me>
2021-10-21 17:42:06 +0200

    Initial Commit

---
+++ iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by Vít Kabele on 21.10.2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
